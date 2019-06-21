c0ce6d48cb985f14502d5ae71ca1a399d68c6888
shic <shic@51kezhi.com>
2019-06-21 13:59:48 +0800

    新增shiro配置

---
+++ demo/src/main/java/com/example/demo/config/shiro/MyRealm.java
@@ -0,0 +1,48 @@
+/**
+ * @ClassName: MyRealm
+ * @Description: TODO(概况)
+ * @author: shic
+ * @date: 2019-05-20 15:53
+ */
+package com.example.demo.config.shiro;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+public class MyRealm extends AuthorizingRealm {
+
+    /**
+     * 授权方法
+     * @param principalCollection
+     * @return
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        System.out.println("权限配置-->MyShiroRealm.doGetAuthorizationInfo()");
+        SimpleAuthorizationInfo authorizationInfo = new SimpleAuthorizationInfo();
+//        UserInfo userInfo  = (UserInfo)principals.getPrimaryPrincipal();
+//        for(SysRole role:userInfo.getRoleList()){
+//            authorizationInfo.addRole(role.getRole());
+//            for(SysPermission p:role.getPermissions()){
+//                authorizationInfo.addStringPermission(p.getPermission());
+//            }
+//        }
+        return authorizationInfo;
+    }
+
+    /**
+     * 认证方法
+     * @param authenticationToken
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
